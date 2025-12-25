9eb7952dac2de83dbbfe1a7f999ca4673670b54f
saifallah1234 <saifallahbellhaj2017@gmail.com>
2025-12-25 20:56:07 +0100

    changing the db.js to connect to the cluster in mongo atlas

--- config/db.js
+++ config/db.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
+const clientOptions = { 
+  serverApi: { version: '1', strict: true, deprecationErrors: true } 
+};
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const uri = process.env.MONGO_URI; 
+    
+    await mongoose.connect(uri, clientOptions);
+    
+    await mongoose.connection.db.admin().command({ ping: 1 });
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log("✅ Successfully connected to MongoDB Atlas (Database: linkedin)!");
     
   } catch (error) {
-    console.error(`❌ Error: ${error.message}`);
+    console.error("❌ MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
